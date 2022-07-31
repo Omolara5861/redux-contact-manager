7dd2c1a7ccb5eb7a834e3a629bfc86c516768e12
Omolara5861 <debbiegterra@gmail.com>
2022-07-31 21:06:36 +0100

    actions

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,29 +1,8 @@
 import React, { Component } from "react";
 import Contact from './Contact';
-export default class Contacts extends Component {
+import {connect  } from 'react-redux';
 
-  state = {
-    contacts: [
-        {
-            id: 1,
-            name: "John Doe",
-            email: "jdoe@gmail.com",
-            phone: "555-555-5555"
-        },
-        {
-            id: 2,
-            name: "Karen Smith",
-            email: "karen@gmail.com",
-            phone: "333-333-3333"
-        },
-        {
-            id: 3,
-            name: "Sherlock Holmes",
-            email: "sherlock@gmail.com",
-            phone: "888-888-8888"
-        }
-    ],
-};
+export default class Contacts extends Component {
 
   render() {
       const { contacts } = this.state;
--- src/reducers/contactReducer.ts
+++ src/reducers/contactReducer.ts
@@ -1,8 +1,35 @@
-const initialState = {};
+import { GET_CONTACTS } from '../actions/types';
+
+const initialState = {
+    contacts: [
+        {
+            id: 1,
+            name: "John Doe",
+            email: "jdoe@gmail.com",
+            phone: "555-555-5555"
+        },
+        {
+            id: 2,
+            name: "Karen Smith",
+            email: "karen@gmail.com",
+            phone: "333-333-3333"
+        },
+        {
+            id: 3,
+            name: "Sherlock Holmes",
+            email: "sherlock@gmail.com",
+            phone: "888-888-8888"
+        }
+    ],
+};
 
 
 export default function contactReducer(state = initialState, action) {
     switch (action.type) {
+        case GET_CONTACTS:
+            return {
+                ...state
+            };
         default:
             return state;
     }
